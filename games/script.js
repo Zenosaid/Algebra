1579d0213f1a14513eebbba7feb0b6f34169c4d9
Zenosaid <119419666+Zenosaid@users.noreply.github.com>
2023-02-15 14:28:07 -0500

    Update script.js

--- games/script.js
+++ games/script.js
@@ -30,3 +30,23 @@ if(month == 3 && day == 1){
     }
     searchInput.addEventListener('input', updateResults);
   };
+const gameList = document.querySelector('.game-list');
+
+// Select all the game elements
+const games = gameList.querySelectorAll('.game');
+
+// Convert the game elements to an array
+const gamesArray = Array.from(games);
+
+// Sort the game elements alphabetically by title
+gamesArray.sort((a, b) => {
+  const aTitle = a.querySelector('.game-title').textContent.trim().toLowerCase();
+  const bTitle = b.querySelector('.game-title').textContent.trim().toLowerCase();
+  return aTitle.localeCompare(bTitle);
+});
+
+// Remove the game elements from the list
+games.forEach(game => game.remove());
+
+// Append the sorted game elements to the list
+gamesArray.forEach(game => gameList.appendChild(game));
