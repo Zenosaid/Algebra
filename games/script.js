65ed8f05ca5165e9e2d3ba903c9f530b9bd01faf
Zenosaid <119419666+Zenosaid@users.noreply.github.com>
2023-09-16 09:23:19 -0400

    er

--- games/script.js
+++ games/script.js
@@ -1,44 +1,57 @@
 // Select the elements
 const gamesContainer = document.querySelector('.games');
-const searchBar = document.querySelector('#search');
-const gameContainer = document.querySelector('.game-detail');
-const gameFrame = gameContainer.querySelector('.frame');
+const searchBar = document.querySelector('.search-bar');
+const gameDetailContainer = document.querySelector('.game-detail');
 
-// Define your CDN base URL
-const cdn = 'https://gitloaf.com/cdn';
+const cdn = 'https://gitloaf.com/cdn'; // Your CDN URL
 
 // Function to create a game container based on game data
 function createGameContainer(game) {
-  const gameContainer = document.createElement('div');
-  gameContainer.className = 'game-container';
-  gameContainer.innerHTML = `
-    <img src="${cdn}/${game.root}/${game.img}" onerror="this.src='./assets/globe.svg'"/>
-    <p>${game.name}</p>
-  `;
+    const gameContainer = document.createElement('div');
+    gameContainer.className = 'game-container';
+    
+    // Create the game content
+    const gameContent = document.createElement('div');
+    gameContent.innerHTML = `
+        <img src="${cdn}/${game.root}/${game.img}" onerror="this.src='./assets/globe.svg'"/>
+        <p>${game.name}</p>
+    `;
 
-  // Add a click event listener to open the game when clicked
-  gameContainer.addEventListener('click', () => {
-    gamesContainer.classList.add('hidden');
-    searchBar.classList.add('hidden');
-    gameFrame.querySelector('iframe').src = `./assets/game?game=${game.root}`;
-  });
+    // Add a click event listener to open the game details when clicked
+    gameContainer.addEventListener('click', () => {
+        // Clear existing game details
+        gameDetailContainer.innerHTML = '';
 
-  return gameContainer;
+        // Create an iframe for the game
+        const gameIframe = document.createElement('iframe');
+        gameIframe.src = `./assets/game?game=${game.root}`;
+
+        // Add the iframe to the game detail container
+        gameDetailContainer.appendChild(gameIframe);
+
+        // Show the game detail container
+        gameDetailContainer.style.display = 'block';
+    });
+
+    // Add the game content to the game container
+    gameContainer.appendChild(gameContent);
+
+    return gameContainer;
 }
 
-// Fetch the games data from a JSON file
+// Fetch the games data from the JSON file
 fetch('./assets/json/games.json')
-  .then((res) => res.json())
-  .then((games) => {
-    // Loop through each game and create a new game container for it
-    games.forEach((game) => {
-      const gameContainer = createGameContainer(game);
-      gamesContainer.appendChild(gameContainer);
+    .then((res) => res.json())
+    .then((games) => {
+        // Loop through each game and create a new game container for it
+        games.forEach((game) => {
+            const gameContainer = createGameContainer(game);
+            gamesContainer.appendChild(gameContainer);
+        });
+    })
+    .catch((e) => {
+        alert('Could not load games');
+        alert(e);
     });
-  })
-  .catch((e) => {
-    alert('Could not load games');
-    alert(e);
-  });
 
 // Rest of your existing code for search and other functionality...
