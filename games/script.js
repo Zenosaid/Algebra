f2309173645611419831fab451aac00ff57a9c9e
Zenosaid <119419666+Zenosaid@users.noreply.github.com>
2023-09-16 10:56:46 -0400

    Update script.js

--- games/script.js
+++ games/script.js
@@ -1,57 +1,58 @@
 // Select the elements
 const gamesContainer = document.querySelector('.games');
-const searchBar = document.querySelector('.search-bar');
-const gameDetailContainer = document.querySelector('.game-detail');
+const searchBar = document.querySelector('.searchbar');
+const gameContainer = document.querySelector('.gamecontainer');
+const gameFrame = gameContainer.querySelector('.frame');
+const gameNav = gameContainer.querySelector('.nav');
 
-const cdn = 'https://gitloaf.com/cdn'; // Your CDN URL
+// Define your GitHub username and repo base URL
+const githubUsername = 'Zenosaid'; // Replace with your GitHub username
+const githubRepoBaseUrl = 'https://github.com/' + githubUsername + '/yourrepo/blob/main'; // Replace 'yourrepo' with your repository name
 
-// Function to create a game container based on game data
-function createGameContainer(game) {
-    const gameContainer = document.createElement('div');
-    gameContainer.className = 'game-container';
-    
-    // Create the game content
-    const gameContent = document.createElement('div');
-    gameContent.innerHTML = `
-        <img src="${cdn}/${game.root}/${game.img}" onerror="this.src='./assets/globe.svg'"/>
-        <p>${game.name}</p>
-    `;
+// Function to create a game icon based on the game data
+function createGameIcon(game) {
+  const gameIcon = document.createElement('div');
+  gameIcon.className = 'game-container';
+  gameIcon.innerHTML = `
+    <img src="${cdn}/${game.root}/${game.img}" onerror="this.src='./assets/globe.svg'"/>
+    <p>${game.name}</p>
+  `;
+  
+  // Add a click event listener to open the game when clicked
+  gameIcon.addEventListener('click', () => {
+    gamesContainer.classList.add('hidden');
+    searchBar.classList.add('hidden');
+    gameContainer.classList.remove('hidden');
+    document.querySelector('.saveItems').classList.add('hidden');
+    document.querySelector('.navbar').classList.add('noshadow');
 
-    // Add a click event listener to open the game details when clicked
-    gameContainer.addEventListener('click', () => {
-        // Clear existing game details
-        gameDetailContainer.innerHTML = '';
+    // Construct the GitHub preview URL for the game's index.html
+    const githubPreviewBaseUrl = 'http://htmlpreview.github.io/?';
+    const gameRoot = game.root;
+    const githubPreviewUrl = `${githubPreviewBaseUrl}${githubRepoBaseUrl}/${gameRoot}/index.html`;
 
-        // Create an iframe for the game
-        const gameIframe = document.createElement('iframe');
-        gameIframe.src = `./assets/game?game=${game.root}`;
+    // Set the src attribute of the iframe to the GitHub preview URL
+    gameFrame.querySelector('iframe').src = githubPreviewUrl;
 
-        // Add the iframe to the game detail container
-        gameDetailContainer.appendChild(gameIframe);
+    gameNav.querySelector('span').textContent = game.name;
+  });
 
-        // Show the game detail container
-        gameDetailContainer.style.display = 'block';
-    });
-
-    // Add the game content to the game container
-    gameContainer.appendChild(gameContent);
-
-    return gameContainer;
+  return gameIcon;
 }
 
-// Fetch the games data from the JSON file
-fetch('./Assets/json/games.json')
-    .then((res) => res.json())
-    .then((games) => {
-        // Loop through each game and create a new game container for it
-        games.forEach((game) => {
-            const gameContainer = createGameContainer(game);
-            gamesContainer.appendChild(gameContainer);
-        });
-    })
-    .catch((e) => {
-        alert('Could not load games');
-        alert(e);
+// Fetch the games data from a JSON file
+fetch('./assets/json/games.json')
+  .then((res) => res.json())
+  .then((games) => {
+    // Loop through each game and create a new game icon for it
+    games.forEach((game) => {
+      const gameIcon = createGameIcon(game);
+      gamesContainer.appendChild(gameIcon);
     });
+  })
+  .catch((e) => {
+    alert('Could not load games');
+    alert(e);
+  });
 
 // Rest of your existing code for search and other functionality...
