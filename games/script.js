2e829fccccd5f4297044fdf34dd070860e3e522f
Zenosaid <119419666+Zenosaid@users.noreply.github.com>
2023-09-16 09:06:54 -0400

    er

--- games/script.js
+++ games/script.js
@@ -1,42 +1,39 @@
 // Select the elements
 const gamesContainer = document.querySelector('.games');
-const searchBar = document.querySelector('.searchbar');
-const gameContainer = document.querySelector('.gamecontainer');
-const gameNav = gameContainer.querySelector('.nav');
+const searchBar = document.querySelector('#search');
+const gameContainer = document.querySelector('.game-detail');
+const gameFrame = gameContainer.querySelector('.frame');
 
+// Define your CDN base URL
+const cdn = 'https://gitloaf.com/cdn';
 
-cdn = 'https://gitloaf.com/cdn'
-// Function to create a game icon based on the game data
-function createGameIcon(game) {
-  const gameIcon = document.createElement('div');
-  gameIcon.className = 'game-container';
-  gameIcon.innerHTML = `
+// Function to create a game container based on game data
+function createGameContainer(game) {
+  const gameContainer = document.createElement('div');
+  gameContainer.className = 'game-container';
+  gameContainer.innerHTML = `
     <img src="${cdn}/${game.root}/${game.img}" onerror="this.src='./assets/globe.svg'"/>
     <p>${game.name}</p>
   `;
-  
+
   // Add a click event listener to open the game when clicked
-  gameIcon.addEventListener('click', () => {
+  gameContainer.addEventListener('click', () => {
     gamesContainer.classList.add('hidden');
     searchBar.classList.add('hidden');
-    gameContainer.classList.remove('hidden');
-    document.querySelector('.saveItems').classList.add('hidden');
-    document.querySelector('.navbar').classList.add('noshadow');
     gameFrame.querySelector('iframe').src = `./assets/game?game=${game.root}`;
-    gameNav.querySelector('span').textContent = game.name;
   });
 
-  return gameIcon;
+  return gameContainer;
 }
 
 // Fetch the games data from a JSON file
 fetch('./assets/json/games.json')
   .then((res) => res.json())
   .then((games) => {
-    // Loop through each game and create a new game icon for it
+    // Loop through each game and create a new game container for it
     games.forEach((game) => {
-      const gameIcon = createGameIcon(game);
-      gamesContainer.appendChild(gameIcon);
+      const gameContainer = createGameContainer(game);
+      gamesContainer.appendChild(gameContainer);
     });
   })
   .catch((e) => {
